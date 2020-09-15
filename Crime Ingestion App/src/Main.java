9ec3e975661353a5019474e59872fdf1dd646081
federicarollo <federica.rollo@libero.it>
2020-09-15 16:14:26 +0200

    Add files via upload

---
+++ Crime Ingestion App/src/Main.java
@@ -0,0 +1,102 @@
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.apache.http.HttpException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+
+public class Main {
+
+	public Main() {
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		//Configuring database connection
+		Object obj = new JSONParser().parse(new FileReader("config.json"));
+    	JSONObject jo = (JSONObject) obj;
+    	JSONObject db = (JSONObject) jo.get("database");
+    	String username = (String) db.get("username");
+    	String password = (String) db.get("password");
+    	Integer num_of_pages = ((Long) (jo.get("num_of_pages"))).intValue();
+		
+		Class.forName("org.postgresql.Driver");
+		String url = "jdbc:postgresql://localhost/postgres?user="+username+"&password="+password;
+		Connection conn = DriverManager.getConnection(url);
+		
+		NewsArticles news_articles = new NewsArticles();
+		
+		System.out.println("ModenaToday");
+		JSONObject obj_MT = (JSONObject) new JSONParser().parse(new FileReader("modenatoday.json"));
+		JSONArray ja_MT = (JSONArray) obj_MT.get("urls");
+		Iterator<JSONObject> iterator_MT = ja_MT.iterator();
+        while(iterator_MT.hasNext()) {
+        	JSONObject obj_iterator = (JSONObject) (iterator_MT.next());
+        	String url_news = (String) obj_iterator.get("url");
+        	String crime_type = (String) obj_iterator.get("crime-type");
+        	System.out.println(url_news);
+        	System.out.println(crime_type);
+        	int insertedNews = news_articles.addNewsMT(conn, num_of_pages, url_news, crime_type);
+        	System.out.println(insertedNews + " news inserted related to " + crime_type + " - ModenaToday");
+        }
+
+        System.out.println("Gazzetta di Modena");        
+        JSONObject obj_GM = (JSONObject) new JSONParser().parse(new FileReader("gazzettadimodena.json"));
+		JSONArray ja_GM = (JSONArray) obj_GM.get("urls");
+		Iterator<JSONObject> iterator_GM = ja_GM.iterator();
+        while(iterator_GM.hasNext()) {
+        	JSONObject obj_iterator = (JSONObject) (iterator_GM.next());
+        	String url_news = (String) obj_iterator.get("url");
+        	String crime_type = (String) obj_iterator.get("crime-type");
+        	System.out.println(url_news);
+        	System.out.println(crime_type);
+        	int insertedNews = news_articles.addNewsGM(conn, num_of_pages, url_news, crime_type);
+        	System.out.println(insertedNews + " news inserted related to " + crime_type + " - Gazzetta di Modena");
+        }
+        
+    	// Trying to find missing geom with LinkedGeoData
+        try {
+			SPARQLHandler sparql = new SPARQLHandler();
+			sparql.findGeomWithSPARQL();
+		} catch (HttpException e){
+			System.out.println(e);
+		}
+		// Searching duplicates
+		System.out.println("---------------------------------------------------------------------");
+		System.out.println("Duplicate detection");
+		// Inserting deduplication algorithm into database
+		Algorithm alg = new Algorithm();
+		alg.addAlgorithm(conn);
+		DuplicateDetection DD = new DuplicateDetection();
+		int InsertedDuplicateNewsDiffNewspaper = DD.DetectionDiffNewspaper(conn);
+		System.out.println("Sono state inserite " +  InsertedDuplicateNewsDiffNewspaper + " notizie duplicate su giornali diversi");
+		int InsertedDuplicateNewsSameNewspaper = DD.DetectionSameNewspaper(conn);
+		System.out.println("Sono state inserite " +  InsertedDuplicateNewsSameNewspaper + " notizie duplicate su stesso giornale");
+		
+		
+		// Statistiche del DB
+		Statistics st = new Statistics();
+		int  nn = st.getNewsNumber(conn);
+		System.out.println("Numero di news totali presenti nel database: "+nn);
+		double  nd = st.getNewsforDay(conn);
+			
+		int ngm = st.getNewsNumberGM(conn);
+		int nmt = st.getNewsNumberMT(conn);
+		HashMap <String, Integer> nft = st.getNewsforTag(conn);
+		
+		System.out.println("Numero medio di news pubblicate al giorno: "+nd);
+		System.out.println("Numero di news totali presenti nel database: "+nn);
+		System.out.println("Numero di news del giornale La Gazzetta di Modena: "+ngm);
+		System.out.println("Numero di news del Giornale ModenaToday: "+nmt);
+		System.out.println("Numero di notizie per tag: "+nft);
+	}
+
+}
